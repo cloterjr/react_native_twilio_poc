978e7b6f9f9c39ddfc83e9c0d18799c7faa9b239
Cloter Gonçalves Barbosa <cbarbosa@viceri.com.br>
2019-07-15 19:42:37 -0300

    Adjusted Main Screen

--- App.js
+++ App.js
@@ -5,8 +5,8 @@ import {
   TwilioVideoParticipantView,
   TwilioVideo
 } from 'react-native-twilio-video-webrtc'
- 
-export default class Example extends Component {
+
+export default class TwilioExample extends Component {
   state = {
     isAudioEnabled: true,
     isVideoEnabled: true,
@@ -16,40 +16,40 @@ export default class Example extends Component {
     roomName: 'bella_teste',
     token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzdlNGI5YjE0OGE1YTg5Njc2ZmUzMTM2YWEzYTVjNWU2LTE1NjMyMjkyNjYiLCJpc3MiOiJTSzdlNGI5YjE0OGE1YTg5Njc2ZmUzMTM2YWEzYTVjNWU2Iiwic3ViIjoiQUMxN2U3YWYxMTIwZjM5NTQyYTViYTZhN2UyZmY3YWI5YyIsImV4cCI6MTU2MzIzMjg2NiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoiYmVsbGFfdGVzdGUiLCJ2aWRlbyI6e319fQ.d4yXsC0L3cB9PE9SqcQkxPTCSz4VCwffXk3g1G9_gcs'
   }
- 
+
   _onConnectButtonPress = () => {
     this.refs.twilioVideo.connect({ roomName: this.state.roomName, accessToken: this.state.token })
     this.setState({status: 'connecting'})
   }
- 
+
   _onEndButtonPress = () => {
     this.refs.twilioVideo.disconnect()
   }
- 
+
   _onMuteButtonPress = () => {
     this.refs.twilioVideo.setLocalAudioEnabled(!this.state.isAudioEnabled)
       .then(isEnabled => this.setState({isAudioEnabled: isEnabled}))
   }
- 
+
   _onFlipButtonPress = () => {
     this.refs.twilioVideo.flipCamera()
   }
- 
+
   _onRoomDidDisconnect = ({roomName, error}) => {
     console.warn("ERROR: ", error)
- 
+
     this.setState({status: 'disconnected'})
   }
- 
+
   _onRoomDidFailToConnect = (error) => {
     console.warn("ERROR: ", error)
- 
+
     this.setState({status: 'disconnected'})
   }
- 
+
   _onParticipantAddedVideoTrack = ({participant, track}) => {
     console.warn("onParticipantAddedVideoTrack: ", participant, track)
- 
+
     this.setState({
       videoTracks: new Map([
         ...this.state.videoTracks,
@@ -57,136 +57,47 @@ export default class Example extends Component {
       ]),
     });
   }
- 
+
   _onParticipantRemovedVideoTrack = ({participant, track}) => {
     console.warn("onParticipantRemovedVideoTrack: ", participant, track)
- 
+
     const videoTracks = this.state.videoTracks
     videoTracks.delete(track.trackSid)
- 
+
     this.setState({videoTracks: { ...videoTracks }})
   }
- 
+
   render() {
     return (
-      <View style={styles.container}>
-        {
-          this.state.status === 'disconnected' &&
-          <View>
-            <Text style={styles.welcome}>
-              React Native Twilio Video
-            </Text>
-            <TextInput
-              style={styles.input}
-              autoCapitalize='none'
-              value={this.state.roomName}
-              onChangeText={(text) => this.setState({roomName: text})}>
-            </TextInput>
-            <TextInput
-              style={styles.input}
-              autoCapitalize='none'
-              value={this.state.token}
-              onChangeText={(text) => this.setState({token: text})}>
-            </TextInput>
-            <Button
-              title="Connect"
-              style={styles.button}
-              onPress={this._onConnectButtonPress}>
-            </Button>
-          </View>
-        }
- 
-        {
-          (this.state.status === 'connected' || this.state.status === 'connecting') &&
-            <View style={styles.callContainer}>
-            {
-              this.state.status === 'connected' &&
-              <View style={styles.remoteGrid}>
-                {
-                  Array.from(this.state.videoTracks, ([trackSid, trackIdentifier]) => {
-                    return (
-                      <TwilioVideoParticipantView
-                        style={styles.remoteVideo}
-                        key={trackSid}
-                        trackIdentifier={trackIdentifier}
-                      />
-                    )
-                  })
-                }
-              </View>
-            }
-            <View
-              style={styles.optionsContainer}>
-              <TouchableOpacity
-                style={styles.optionButton}
-                onPress={this._onEndButtonPress}>
-                <Text style={{fontSize: 12}}>End</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.optionButton}
-                onPress={this._onMuteButtonPress}>
-                <Text style={{fontSize: 12}}>{ this.state.isAudioEnabled ? "Mute" : "Unmute" }</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={styles.optionButton}
-                onPress={this._onFlipButtonPress}>
-                <Text style={{fontSize: 12}}>Flip</Text>
-              </TouchableOpacity>
-              <TwilioVideoLocalView
-                enabled={true}
-                style={styles.localVideo}
-              />
-            </View>
-          </View>
-        }
- 
+      <View style={{ flex: 1 }}>
+      <View style={{ flex: 3 }}>
         <TwilioVideo
           ref="twilioVideo"
           onRoomDidConnect={ this._onRoomDidConnect }
           onRoomDidDisconnect={ this._onRoomDidDisconnect }
-          onRoomDidFailToConnect= { this._onRoomDidFailToConnect }
+          onRoomDidFailToConnect= { (error)=>this._onRoomDidFailToConnect(error) }
           onParticipantAddedVideoTrack={ this._onParticipantAddedVideoTrack }
           onParticipantRemovedVideoTrack= { this._onParticipantRemovedVideoTrack }
         />
+ 
+        <TwilioVideoLocalView style={{flex: 1, backgroundColor: '#FFFF00'}} />
+ 
+        <TwilioVideoParticipantView style={{flex: 1, backgroundColor: '#FF00FF'}} />
       </View>
+      <View style={{flex:1}}>
+              <Button
+              style={{fontSize: 20, padding: 10, color: 'green'}}
+              onPress={this._onConnectButtonPress}
+              title="Connect">
+            </Button>
+     
+            <Button
+              style={{fontSize: 20, padding: 10, color: 'red'}}
+              onPress={this._onEndButtonPress}
+              title="Disconnect">
+            </Button>
+            </View>
+            </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  input: {
-
-  },
-  button: {
-
-  },
-  callContainer: {
-
-  },
-  remoteGrid: {
-
-  },
-  remoteVideo: {
-
-  },
-  optionsContainer: {
-
-  },
-  optionButton: {
-
-  },
-  localVideo: {
-
-  }
-});
